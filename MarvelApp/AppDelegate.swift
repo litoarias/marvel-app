3a6c64001d55a356ee7b307efc268227294f4ad4
Hipolito Arias <hipolito.arias@kairosds.com>
2020-12-04 11:56:16 +0100

    starting project config

---
+++ MarvelApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MarvelApp
+//
+//  Created by Hipolito Arias on 4/12/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+
+target 'MarvelApp' do
+	use_frameworks!
+	pod 'Alamofire'
+	pod 'PromiseKit/CorePromise'
+	pod 'Sourcery'
+end
