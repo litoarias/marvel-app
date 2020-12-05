31839c6b60feacc9ec45faeb6875f0cece207d7e
Hipolito Arias <hipolito.arias@kairosds.com>
2020-12-05 14:07:46 +0100

    add loading cell

--- MarvelApp/LoadingTableViewCell.swift
+++ MarvelApp/LoadingTableViewCell.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class LoadingTableViewCell: UITableViewCell {
 
+	var activityIndicator = UIActivityIndicatorView()
+	static let className = String(describing: LoadingTableViewCell.self)
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+		
+		contentView.addSubview(activityIndicator)
+		activityIndicator.startAnimating()
+		activityIndicator.style = .gray
+		activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+		activityIndicator.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+		activityIndicator.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+		activityIndicator.heightAnchor.constraint(equalToConstant: 48).isActive = true
+		activityIndicator.widthAnchor.constraint(equalToConstant: 48).isActive = true
+		activityIndicator.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6).isActive = true
+		activityIndicator.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -6).isActive = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- MarvelApp/ViewController.swift
+++ MarvelApp/ViewController.swift
@@ -1,48 +1,115 @@
-
 import UIKit
-import Foundation
 import PromiseKit
 import Alamofire
+import PagedLists
+
+let defaultLimit = 60
 
 class ViewController: UIViewController {
-	let apiSession = Session()
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		view.backgroundColor = .red
+	private let apiSession = Session()
+	private var currentPage: Page = Page(limit: defaultLimit, offset: 0)
+	private var stop: Bool = false
+	
+	var tableView = PagedTableView(frame: .zero)
+	var characterList = [Character]()
+	
+	override func loadView() {
+		super.loadView()
 		
+		view.addSubview(tableView)
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+		tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+		tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+		tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+		tableView.register(LoadingTableViewCell.self, forCellReuseIdentifier: LoadingTableViewCell.className)
+		tableView.dataSource = self
+		tableView.rowHeight = UITableView.automaticDimension
+		tableView.estimatedRowHeight = UITableView.automaticDimension
+		tableView.elementsPerPage = defaultLimit
+		tableView.updateDelegate = self
 		
+	}
+	
+	func cleanFunc(_ completion: ((Int, NSError?) -> Void)?) {
 		firstly {
 			self.getCharacters()
-		}.then { characters in
-			self.sendSecondRequest()
-		}.done {
-			debugPrint($0)
+		}.then {
+			self.reloadItems($0, completion: completion ?? {_,_ in})
 		}.catch {
 			debugPrint($0)
 		}.finally {
 			debugPrint("Finished")
 		}
-		
 	}
 	
 	func getCharacters() -> Promise<CharacterRequest> {
 		let (promise, seal) = Promise<CharacterRequest>.pending()
-		#warning("DYNAMIC PARAMS PLEASE!")
-		apiSession.request(.getCharacters(20, 0)).done { (characters: CharacterRequest) in
-			seal.fulfill(characters)
+		apiSession.request(.getCharacters(currentPage)).done { (characters: CharacterRequest) in
+			seal.fulfill((characters))
 		}.catch { error in
 			seal.reject(error)
 		}
 		return promise
 	}
 	
-	func sendSecondRequest() -> Promise<String> {
-		let (promise, seal) = Promise<String>.pending()
-		DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
-			seal.fulfill(("Tu tutu utut utut ut!!"))
-		}
+	func reloadItems(_ list: CharacterRequest, completion: @escaping (Int, NSError?) -> Void) -> Promise<Int> {
+		let (promise, seal) = Promise<Int>.pending()
+		completion(list.data?.count ?? 0, nil)
+		currentPage.offset += list.data?.count ?? 0
+		self.characterList += list.data?.results ?? []
+		self.tableView.reloadData()
+		seal.fulfill(list.data?.count ?? 0)
 		return promise
 	}
 	
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		if indexPath.section == 0 {
+			return UITableView.automaticDimension
+		} else {
+			return 65
+		}
+	}
+}
+
+// MARK: - Data Source
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if section == 0 {
+			return characterList.count
+		} else if section == 1 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 2
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if indexPath.section == 0 {
+			let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: LoadingTableViewCell.self), for: indexPath) as UITableViewCell
+			cell.textLabel?.text = "\(indexPath.row): \(self.characterList[indexPath.row].name ?? "\(indexPath.row)")"
+			return cell
+		} else {
+			let cell = tableView.dequeueReusableCell(withIdentifier: LoadingTableViewCell.className, for: indexPath) as? LoadingTableViewCell
+			cell?.activityIndicator.startAnimating()
+			return cell ?? LoadingTableViewCell()
+		}
+	}
+}
+
+extension ViewController: PagedTableViewDelegate {
+	
+	func tableView(_ tableView: PagedTableView, needsDataForPage page: Int, completion: @escaping (Int, NSError?) -> Void) {
+		cleanFunc(completion)
+	}
 }
