de093b16d19002af5d0f64d50781eae67f335969
Hipolito Arias <hipolito.arias@kairosds.com>
2021-08-28 02:13:19 +0200

    add mix

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -2,6 +2,19 @@
 
 set -eo pipefail
 
+#check if env-vars.sh exists
+echo "$PWD/env-vars.sh"
+
+if [ -f $PWD/env-vars.sh ]
+then
+echo "I am here"
+source $PWD/env-vars.sh
+fi
+
+#no `else` case needed if the CI works as expected
+$PWD/Pods/Sourcery/bin/sourcery --templates $PWD/MarvelApp --sources $PWD --output $PWD/MarvelApp/Stuff/Environment --args publicApiKey=$MARVEL_PUBLIC_API_KEY,privateApiKey=$MARVEL_PRIVATE_API_KEY
+
+
 xcodebuild -workspace MarvelApp.xcworkspace  \
            -scheme MarvelApp-Release \
            build
